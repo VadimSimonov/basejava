969c460f6792d6ff1e7f4affc3849b86df8f0a92
VadimSimonov <dark-vadim@mail.ru>
2017-06-20 16:32:36 +0300

    HW4 SortedArrayStorageTest compete

--- test/ru/javawebinar/basejava/storage/SortedArrayStorageTest.java
+++ test/ru/javawebinar/basejava/storage/SortedArrayStorageTest.java
@@ -1,23 +1,32 @@
 package ru.javawebinar.basejava.storage;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
-
-import static org.junit.Assert.*;
+import ru.javawebinar.basejava.model.Resume;
 
 /**
  * Created by simonov on 6/21/17.
  */
 public class SortedArrayStorageTest extends AbstractArrayStorageTest {
-    @Test
-    public void fillDeletedElement() throws Exception {
-    }
+    private SortedArrayStorage storage = new SortedArrayStorage();
+    private static final String UUID_1 = "uuid1";
+    private static final String UUID_2 = "uuid2";
+    private static final String UUID_3 = "uuid3";
 
-    @Test
-    public void insertElement() throws Exception {
-    }
 
+    @Override
+    @Before
+    public void setUp() throws Exception {
+        storage.clear();
+        storage.save(new Resume(UUID_1));
+        storage.save(new Resume(UUID_2));
+        storage.save(new Resume(UUID_3));
+
+    }
     @Test
     public void getIndex() throws Exception {
+        Assert.assertEquals(0,storage.getIndex(UUID_1));
     }
 
 }
