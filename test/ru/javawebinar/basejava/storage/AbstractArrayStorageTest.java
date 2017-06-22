a8a5f0e1087459db8ef202ce035d18b1226002d5
VadimSimonov <dark-vadim@mail.ru>
2017-06-22 12:39:22 +0300

    HW4 JUnit complete

--- test/ru/javawebinar/basejava/storage/AbstractArrayStorageTest.java
+++ test/ru/javawebinar/basejava/storage/AbstractArrayStorageTest.java
@@ -5,13 +5,17 @@ import org.junit.Before;
 import org.junit.Test;
 import ru.javawebinar.basejava.exception.NotExistStorageException;
 import ru.javawebinar.basejava.model.Resume;
+import static org.hamcrest.CoreMatchers.*;
 
 public abstract class AbstractArrayStorageTest {
     private Storage storage = new ArrayStorage();
+    private Storage storageTest = new ArrayStorage();
 
     private static final String UUID_1 = "uuid1";
     private static final String UUID_2 = "uuid2";
     private static final String UUID_3 = "uuid3";
+    private static final Resume UpdateUiid = new Resume(UUID_1);
+    private static final Resume SaveUiid = new Resume("muhaha");
 
     @Before
     public void setUp() throws Exception {
@@ -28,32 +32,39 @@ public abstract class AbstractArrayStorageTest {
 
     @Test
     public void clear() throws Exception {
-
+        storage.clear();
+        Assert.assertThat(storage.getAll(),is(storageTest.getAll()));
     }
 
     @Test
     public void update() throws Exception {
+        storage.update(UpdateUiid);
+        Assert.assertEquals(UpdateUiid,storage.get(String.valueOf(UpdateUiid)));
 
     }
 
     @Test
     public void getAll() throws Exception {
-
+        Assert.assertEquals(new Resume(UUID_1),storage.get(UUID_1));
+        Assert.assertEquals(new Resume(UUID_2),storage.get(UUID_2));
+        Assert.assertEquals(new Resume(UUID_3),storage.get(UUID_3));
     }
 
     @Test
     public void save() throws Exception {
-
+        storage.save(SaveUiid);
+        Assert.assertEquals(SaveUiid,storage.get(String.valueOf(SaveUiid)));
     }
 
-    @Test
+    @Test(expected = NotExistStorageException.class)
     public void delete() throws Exception {
-
+        storage.delete(String.valueOf(UpdateUiid));
+        Assert.assertThat(UpdateUiid,not(equalTo(storage.get(String.valueOf(UpdateUiid)))));
     }
 
     @Test
     public void get() throws Exception {
-
+        Assert.assertThat(UpdateUiid,equalTo(storage.get(String.valueOf(UpdateUiid))));
     }
 
     @Test(expected = NotExistStorageException.class)
