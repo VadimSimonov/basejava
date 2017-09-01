c5aeba38e20be70a9a6478e15d27b75c79933cce
VadimSimonov <dark-vadim@mail.ru>
2017-09-01 16:27:36 +0300

    HW13 refact complete mini fix

--- src/ru/javawebinar/basejava/storage/SqlStorage.java
+++ src/ru/javawebinar/basejava/storage/SqlStorage.java
@@ -15,8 +15,8 @@ import java.util.List;
 
 public class SqlStorage implements Storage {
     private final ConnectionFactory connectionFactory;
-    int colSize=0;
-    ResultSet rs;
+    private int colSize=0;
+    private ResultSet rs;
     Resume r;
 
     public SqlStorage(String dbUrl, String dbUser, String dbPassword) {
@@ -48,21 +48,6 @@ public class SqlStorage implements Storage {
         });
         return r;
     }
-
-/*
-        try (Connection conn = connectionFactory.getConnection();
-             PreparedStatement ps = conn.prepareStatement("SELECT * FROM resume r WHERE r.uuid =?")) {
-            ps.setString(1, uuid);
-            ResultSet rs = ps.executeQuery();
-            if (!rs.next()) {
-                throw new NotExistStorageException(uuid);
-            }
-            return new Resume(uuid, rs.getString("full_name"));
-        } catch (SQLException e) {
-            throw new StorageException(e);
-        }
-    }
-    */
 
     @Override
     public void update(Resume r) {
